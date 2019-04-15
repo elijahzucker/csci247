b5027c4c35fc564bb8d6eb0dfa852df80c6e8eb6
elijahzucker <38961810+elijahzucker@users.noreply.github.com>
2019-04-15 16:47:10 -0700

    added almost everything for lab1

--- CSCI247/Labs/Lab1/Lab1.c
+++ CSCI247/Labs/Lab1/Lab1.c
@@ -2,6 +2,15 @@
     Course: CSCI 247 -Spring 2019
     File Name Lab1.c
 
+    Preproccessor directives: Include/Define,
+    Foward Declaration: 
+    Macros:
+    Functions:
+    Escape Charecter: these tell the compiler that the following bit in say a print statement will
+    be an instruction like \n calling for a paragraph break.
+    Operators:
+    Keywords:
+    Printf: 
 */
 
 #include<stdio.h>
@@ -9,18 +18,31 @@
 
 #define CHAR_DATA_TYPE "char"
 #define INT_DATA_TYPE "int"
+
 char* GetStringFromStdin(char* str);
 
 void DataTypeSizes(void);
-void DisplayString( const char* str);
+void DisplayString(const char* str);
 
-int main(){
+int main(int argc, char* argv[]){
+    printf("argc %d\n", argc - 1);
+    for(int i = 0; i < sizeof(argc); i++){
+        printf("argv[%d] %s\n",i , argv[i]);
+    }
     //printf("Wow! This is my first C program!\r\n");
     char str[100];
-    fgets(str, 100, stdin);
+    char* strPtr;
+    fputs(GetStringFromStdin(str), stdout);
+    DisplayString(str);
     DataTypeSizes();
     return(0);
 }
+
+char* GetStringFromStdin(char* str){
+    printf("Enter a thing: ");
+    return fgets(str, 100, stdin);
+}
+
 
 void DataTypeSizes(void){
     printf("\"%s\" is a standard C datatype. Size of a \"%s\" data type is=%ld\r\n",
@@ -29,4 +51,6 @@ void DataTypeSizes(void){
     INT_DATA_TYPE, INT_DATA_TYPE, sizeof(int));
 }
 
-//void Display( const char* str)
+void DisplayString(const char* str){
+        printf("%s \n", str);
+}
