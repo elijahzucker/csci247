019242a6d4d4f4011bacf121ead62bce838470f7
elijahzucker <38961810+elijahzucker@users.noreply.github.com>
2019-04-24 15:08:45 -0700

    all but 2 and 9

--- Labs/Lab3/Lab3.c
+++ Labs/Lab3/Lab3.c
@@ -6,21 +6,29 @@ int identifyUniqueInteger(int arrayCount, int*Array);
 int reverseBits(int var);
 bool onlyOneBitSet(int);
 bool onlyOneBitSetInEvenPosition(int);
-int modWithoutUsingModOperator(int numerator, int denominator);
+int modWithoutUsingModOperator(int n, int d);
 int swapNibbles(int);
 int numberOfOperationsRequired(int);
 
 int main(int argc, char *argv[]) {
     int i;
+    //int r;                                                  //for mod w/out
     printf("Enter a number: ");
     scanf("%d",&i);
+    //printf("Enter a second number: ");                      //for mod w/out
+    //scanf("%d",&r);
     //printf("%d \n", countSetBits(i));                     //Task 1
     //printf("%d \n", reverseBits(i));                      //Task 3
-    printf("%d \n", onlyOneBitSet(i));
+    //printf("%d \n", onlyOneBitSet(i));                    //Task 4
+    //printf("%d \n", onlyOneBitSetInEvenPosition(i));      //Task 5
+    //printf("%d \n", modWithoutUsingModOperator(i, r));    //Task 6
+    //printf("%d \n", swapNibbles(i));                      //Task 7
+    printf("%d \n", numberOfOperationsRequired(i));                      //Task 8
 
     return 0;
 }
 
+//Task 1
 int countSetBits(unsigned int var){
     unsigned int i = 0;
     while(var){
@@ -30,10 +38,12 @@ int countSetBits(unsigned int var){
     return i;
 }
 
+//Task 2
 int identifyUniqueInteger(int arrayCount, int*Array){
 
 }
 
+//Task 3
 int reverseBits(int var){
     int i = 0;
     while(var){
@@ -46,32 +56,54 @@ int reverseBits(int var){
     return i;
 }
 
+//Task 4
 bool onlyOneBitSet(int var){
-    /*int i;
-    while(var && i < 1){
-        i += var & 1;
-        var >>= 1;
-    }
-    printf("the value of i: %d", i);
-    if(i == 1){
-        return true;
-    }
-    return false;*/
     return var && !(var & (var-1));
 }
-/*
-bool onlyOneBitSetInEvenPosition(int){
 
+//Task 5
+bool onlyOneBitSetInEvenPosition(int var){
+    int i = 0;
+    if(onlyOneBitSet(var) == 0){
+        return 0;
+    }
+    while(onlyOneBitSet(var) == 1){
+        var >>= 1;
+        i++;
+    }
+    if(i % 2 == 0){
+        return 1;
+    }
+    return 0;
 }
 
-int modWithoutUsingModOperator(int numerator, int denominator){
-
+//Task 6
+int modWithoutUsingModOperator(int n, int d){
+    int q = n / d;
+    int p = q * d;
+    int m = n - p;
+    return m;
 }
 
-int swapNibbles(int){
-
+//Task 7
+int swapNibbles(int var){
+    return((var & 0x0F) << 4 | (var & 0xF0) >> 4);
 }
 
-int numberOfOperationsRequired(int){
-    
-}*/
+//Task 8
+int numberOfOperationsRequired(int var){
+    int count = 0;
+    if(var <= 1){
+        return count;
+    }
+    while(var > 1){
+        if(onlyOneBitSet(var)){
+            var = var - (var >> 1);
+            count++;
+            continue;
+        }
+        var = var/2;
+        count++;
+    }
+    return count;
+}
