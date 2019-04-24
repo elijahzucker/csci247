2058717e3fb310f8efe16e5aa3f9567871ce656c
elijahzucker <38961810+elijahzucker@users.noreply.github.com>
2019-04-24 13:40:26 -0700

    2 tasks completed

--- Labs/Lab3/Lab3.c
+++ Labs/Lab3/Lab3.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>        //bool was unidentified
 
 int countSetBits(unsigned int var);
 int identifyUniqueInteger(int arrayCount, int*Array);
 int reverseBits(int var);
+bool onlyOneBitSet(int);
+bool onlyOneBitSetInEvenPosition(int);
+int modWithoutUsingModOperator(int numerator, int denominator);
+int swapNibbles(int);
+int numberOfOperationsRequired(int);
 
 int main(int argc, char *argv[]) {
-    /*int i;                                                //Task 1
-    printf("Enter a number to check # of set bits: ");      //Task 1
-    scanf("%d",&i);                                         //Task 1
-    printf("%d \n", countSetBits(i));*/                     //Task 1
+    int i;
+    printf("Enter a number: ");
+    scanf("%d",&i);
+    //printf("%d \n", countSetBits(i));                     //Task 1
+    printf("%d \n", reverseBits(i));
+
     return 0;
 }
 
@@ -26,5 +34,33 @@ int identifyUniqueInteger(int arrayCount, int*Array){
 }
 
 int reverseBits(int var){
-    
+    int i = 0;
+    while(var){
+        i <<= 1;
+        if(var & 1 == 1){
+            i ^= 1;
+        }
+        var >>= 1;
+    }
+    return i;
 }
+/*
+bool onlyOneBitSet(int){
+
+}
+
+bool onlyOneBitSetInEvenPosition(int){
+
+}
+
+int modWithoutUsingModOperator(int numerator, int denominator){
+
+}
+
+int swapNibbles(int){
+
+}
+
+int numberOfOperationsRequired(int){
+    
+}*/
