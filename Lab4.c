1f5ff4b725444bcba39f45cf7f01c6e84de48d14
elijahzucker <38961810+elijahzucker@users.noreply.github.com>
2019-05-06 16:53:12 -0700

    lab4 donish

--- Lab4.c
+++ Lab4.c
@@ -6,19 +6,23 @@ typedef unsigned float_bits;
 int floatClass(float_bits f);
 float_bits float_negate(float_bits f);
 float_bits float_absval(float_bits f);
+float_bits float_twice(float_bits f);
+float_bits float_half(float_bits f);
 
 int main(int argc, char *argv[]) {
     unsigned int tVar = 0x41;
     int i = atoi(argv[1]);
-	printf("FloatClass Rest: %u \n", floatClass((float)i));
-    printf("FloatClass Rest: %u \n", float_negate((float)i));
-    printf("FloatClass Rest: %u \n", float_absval((float)i));
+	printf("FloatClass Test: %u \n", floatClass((float)i));
+    printf("FloatNegate Test: %d \n", float_negate((float)i));
+    printf("FloatAbsval Test: %d \n", float_absval((float)i));
+    printf("FloatTwice Test: %d \n", float_twice((float)i));
+    printf("FloatHalf Test: %d \n", float_half((float)i));      //truncating
 	return 0;
 }
 
 //Task 1
 int floatClass(float_bits f){
-    //int sign = f>>31;
+    int sign = f>>31;
     int expo = (f>>23) & 0xff;
     int mant = f & 0x7fffff;
 
@@ -30,7 +34,7 @@ int floatClass(float_bits f){
         return 1;
     }
 
-    else if(expo == 0xff && mant != 0){     //NaN
+    else if(expo == 0xff && f != f){     //NaN
         return 2;
     }
 
@@ -45,18 +49,85 @@ int floatClass(float_bits f){
 
 //Task 2
 float_bits float_negate(float_bits f){
+    if(floatClass(f) ==  0 || floatClass(f) == 1 || floatClass(f) == 2){
+        return f;
+    }
     int sign = f>>31;
-    if(floatClass(f) == 3 || floatClass(f) == 4)
-        return f^0x80000000;
-    return f;
+    int x = 1<<31;
+    return 0x80000000 - f^x;
 }
 
 //Task 3
 float_bits float_absval(float_bits f){
-    if(floatClass(f) == 2 || f > 0 || floatClass(f) == 0){
+    if(floatClass(f) ==  0 || floatClass(f) == 1 || floatClass(f) == 2){
         return f;
     }
-    else{
-        return float_absval(f);
+    float_bits i = float_negate(f);
+    int sign = f>>31;
+    if(sign > 0){
+        return i;
     }
+    return f;
+}
+
+//Task 4
+float_bits float_twice(float_bits f){
+    int flag = 0;
+    if(f>>31 > 0){
+        flag = 1;
+        f = float_absval(f);
+    }
+    int sign = f>>31;
+    int expo = (f>>23) & 0xff;
+    int mant = f & 0x7fffff;
+
+    if(floatClass(f) ==  0 || floatClass(f) == 1 || floatClass(f) == 2){
+        return f;
+    }
+
+    if(floatClass(f) == 3){
+        mant <<= 1;
+    }
+    else if(expo == 0xff -1){
+        expo = 0xff;
+        mant = 0;
+    }
+    else{
+        expo += 1;
+    }
+    if(flag == 1){
+        return float_negate((sign<<31) | (expo<<23) | mant);
+    }
+    return (sign<<31) | (expo<<23) | mant;
+}
+
+//Task 5
+float_bits float_half(float_bits f){
+    int flag = 0;
+    if(f>>31 > 0){
+        flag = 1;
+        f = float_absval(f);
+    }
+    int sign = f>>31;
+    int expo = (f>>23) & 0xff;
+    int mant = f & 0x7fffff;
+
+    if(floatClass(f) ==  0 || floatClass(f) == 1 || floatClass(f) == 2){
+        return f;
+    }
+
+    if(floatClass(f) == 3){
+        mant >>= 1;
+    }
+    else if(expo == 0xff -1){
+        expo = 0xff;
+        mant = 0;
+    }
+    else{
+        expo -= 1;
+    }
+    if(flag == 1){
+        return float_negate((sign<<31) | (expo<<23) | mant);
+    }
+    return (sign<<31) | (expo<<23) | mant;
 }
